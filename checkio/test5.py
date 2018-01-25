f6ad266fc72b40a7e79834e942394a71700cd869
xuan <kxegdyx@163.com>
2018-01-25 17:51:16 +0800

    change
    change

--- checkio/test5.py
+++ checkio/test5.py
@@ -21,4 +21,7 @@ def long_repeat(line):
     return max(L,default = 0)
 
 if __name__ == '__main__':
-    print(long_repeat("abababaab"))
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert long_repeat('sdsffffse') == 4, "First"
+    assert long_repeat('ddvvrwwwrggg') == 3, "Second"
+    print('"Run" is good. How is "Check"?')
